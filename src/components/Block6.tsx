1ef29cd5bd02fd94be3d9bcc5980180eb1126fdf
Nursultan Ulan uulu <ulanuulunursultan36@gmail.com>
2025-12-17 15:39:53 +0600

    test block6

--- src/components/Block6.tsx
+++ src/components/Block6.tsx
@@ -1,25 +1,26 @@
-// import video from '../assets/3192082-uhd_3840_2160_25fps.mp4'
+import video from '../assets/3192082-uhd_3840_2160_25fps.mp4'
 
 const Block6 = () => {
 	return (
-		<section className=' min-h-screen overflow-hidden bg-blue-500 z-10'>
+		<section className='relative h-[70vh] overflow-hidden'>
 			{/* Видео фон */}
-			{/* <div className='absolute inset-0 z-0'>
+			<div className='absolute inset-0 z-0'>
 				<video
 					autoPlay
 					muted
 					loop
 					playsInline
-					preload='auto'
-					className='absolute w-full h-full object-cover'
+					className='w-full h-full object-cover'
 				>
 					<source src={video} type='video/mp4' />
 				</video>
-				<div className='absolute inset-0 bg-black/60' />
-			</div> */}
 
-			{/* Контент */}
-			<div className='relative z-10 flex items-center justify-center min-h-screen px-4'>
+				{/* Затемнение поверх видео */}
+				<div className='absolute inset-0 bg-black/60' />
+			</div>
+
+			{/* Контент поверх видео */}
+			<div className='relative z-10 flex h-full items-center justify-center px-4'>
 				<div className='max-w-[600px] w-full text-center'>
 					<h2 className='text-white text-5xl md:text-6xl font-bold leading-tight'>
 						Přistupuj ke sportu jako k trhu.
